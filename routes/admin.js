a367f59e80f8c247329ae3ae6a155938fb74402a
46517115@qq.com <46517115@qq.com>
2015-10-24 08:08:17 +0800

    CREATE

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,8 @@ var adminRoute = express();
 
 adminRoute.use('/',function(req,res,next){
 	console.log('admin /');
-	res.send('Admin');
+	res.render('admin/index',{title:"admin"});
+	//res.send('Admin');
 });
 
 module.exports = adminRoute;
--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ var apiRoute = express();
 
 apiRoute.use('/',function(req,res,next){
 	console.log('api /');
+	res.render('api/index',{title:"api"});
 	res.send('Api');
 });
 
